2ca60a9b2c7a8dc3ccd2492cc3d4767462e828fd
BravorAndres <Bravor.andres24@gmail.com>
2024-08-29 09:28:15 -0500

    auth controller añadido

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        //validas datos
+        $credentials = $request->only('email','password');
+
+        //validar y generar JWT
+
+        if(!$token = Auth::attempt($credentials)){
+            return response()->json(['error'=>'Unauthorized'],401);
+
+        } else {
+            return $this->respondWithToken($token);
+        }
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'acces_token'=>$token,
+            'token_type'=> 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
